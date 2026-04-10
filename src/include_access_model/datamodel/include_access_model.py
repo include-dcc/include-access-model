753812a386df03f144478afc988bc5081e9cc4e2
Robert Carroll <Robert.Carroll@vumc.org>
2026-04-10 14:33:47 -0500

    📝 Add ontology recommendations
    Add some recommendations for the following request:
    Sample Type
    Processing
    Storage Method
    Organism Type

--- src/include_access_model/datamodel/include_access_model.py
+++ src/include_access_model/datamodel/include_access_model.py
@@ -1,5 +1,5 @@
 # Auto generated from include_access_model.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-03-12T11:07:16
+# Generation date: 2026-04-10T14:31:50
 # Schema: include-access-model
 #
 # id: https://includedcc.org/include-access-model
