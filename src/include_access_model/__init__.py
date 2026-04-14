86f9dcb7ab56a4cd31855b4b63e2e4fff405800f
Robert Carroll <Robert.Carroll@vumc.org>
2026-04-14 13:11:42 -0500

    ⚠️ Remove pydantic model
    Step 1 to ignoring generated pydantic module

--- src/include_access_model/__init__.py
+++
@@ -1,5 +0,0 @@
-try:
-    from include_access_model._version import __version__, __version_tuple__
-except ImportError:  # pragma: no cover
-    __version__ = "0.0.0"
-    __version_tuple__ = (0, 0, 0)
